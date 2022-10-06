d70cd7590473bdea590507d85a246b3b75fa9ff0
Catherine Dubois <catherine.dubois@ensiie.fr>
2022-10-06 10:31:28 +0200

    Random generation with QuickChick

--- Coq_lib/LNDT/PerfectTree.v
+++ Coq_lib/LNDT/PerfectTree.v
@@ -26,3 +26,36 @@ Definition pt_in_prop     n := in_prop     _ (any_all_able _ (pt_spreadable n)).
 Definition pt_empty       n := empty       _ (any_all_able _ (pt_spreadable n)).
 Definition pt_dec_in_prop n := dec_in_prop _ (any_all_able _ (pt_spreadable n)).
 Definition pt_dec_eq      n := dec_eq      _ (eq_able _ (pt_spreadable n)).
+
+From QuickChick Require Import QuickChick Tactics.
+
+Module DoNotation.
+Notation "'do!' X <- A ; B" :=
+  (bindGen A (fun X => B))
+    (at level 200, X ident, A at level 100, B at level 200).
+End DoNotation.
+
+Import DoNotation.
+
+Open Scope string.
+
+Definition STuple : forall n, Printable (Tuple n). 
+unfold Printable. induction n ;  intros; simpl.
++ exact X.
++ specialize (IHn A X).
+exact showPair.
+Defined.
+
+Instance showLNDT_Tuplen {n : nat} {A : Type} `(sh : Show A) : Show (LNDT (Tuple n) A) :=
+{|
+  show  := @lndt_print (Tuple n) (STuple n) A sh
+|}.
+
+
+Close Scope string.
+           
+Definition tuple_n_gensized n := lndt_gen_sized (tuple_gensized n) nat arbitrarySized.
+
+Sample (tuple_n_gensized 1 10).
+Sample (tuple_n_gensized 2 10).
+Sample (tuple_n_gensized 3 10).
